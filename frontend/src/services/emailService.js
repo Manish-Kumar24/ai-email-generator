782879b5c103045d0932cb997fa697b6d41c231c
Manish284 <manishkumar202209@gmail.com>
2026-06-18 09:50:51 +0530

    url updated

--- frontend/src/services/emailService.js
+++ frontend/src/services/emailService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "https://ai-email-generator-7dvw.onrender.com";
 
 export const generateEmail = async (payload) => {
     const response = await axios.post(
